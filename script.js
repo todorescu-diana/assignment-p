d0d3634e52754ba3b933de88badb86a6dd27b26d
Diana Todorescu <todorescu.diana@gmail.com>
2021-02-23 23:08:00 +0200

    populat dinamic + optiune stergere + apel server => ecrane mici

--- script.js
+++ script.js
@@ -39,29 +39,102 @@ let data = [
     }
 ];
 
-window.onload = function(){
-    let table = document.querySelector("#tableID tbody");
-    let rowTemp = document.getElementById("rowModel");
-    data 
-        .map( function (o) {
-            let cloneRow = rowTemp.content.cloneNode(true);
+function dataCollect(){
 
-            cloneRow.querySelectorAll("tr .tdclass")[0].innerHTML = o.productName;
-            cloneRow.querySelectorAll("tr .tdclass")[1].innerHTML = o.productCode;
-            cloneRow.querySelectorAll("tr .tdclass")[2].innerHTML = o.productAvailable;
-            cloneRow.querySelectorAll("tr .tdclass")[3].innerHTML = o.productPrice;
-            cloneRow.querySelectorAll("tr .tdclass")[4].innerHTML = o.productRating;
+    if( $( window ).width() <= 500 ){
+        let element = document.createElement('li');
+        element.classList.add("element");
+        let unorderedList = document.getElementById("tableID");
+        unorderedList.appendChild(element);
+        let divTemp = document.getElementById("divModel");
+        data
+            .map( function (o) {
+                let cloneDiv = divTemp.content.cloneNode(true);
+    
+                cloneDiv.querySelectorAll("p")[0].innerHTML = o.productName;
+                cloneDiv.querySelectorAll("p")[1].innerHTML = o.productCode;
+                cloneDiv.querySelectorAll("p")[2].innerHTML = o.productAvailable;
+                cloneDiv.querySelectorAll("p")[3].innerHTML = o.productPrice;
+                cloneDiv.querySelectorAll("p")[4].innerHTML = o.productRating;
+    
+                let btnDelete = cloneDiv.querySelector("button.divDelete");
+    
+                btnDelete.onclick = function (){
+                    let index = data.indexOf(o);
+                    data.splice(index, 1);
+                    document.getElementsByClassName("contentDiv")[index+1].remove();
+                }
+                return cloneDiv;
+            })
+                .forEach ( function (cloneDiv) { return element.appendChild(cloneDiv); } );
 
-            let btnDelete = cloneRow.querySelector("button.delete");
+        console.log("small");
+    }
 
-            btnDelete.onclick = function (){
-                let index = data.indexOf(o);
-                data.splice(index, 1);
-                console.log(data);
-                document.getElementsByTagName("tr")[index+1].remove();
-            }
+    else{
+        let table = document.querySelector("#tableID tbody");
+        let rowTemp = document.getElementById("rowModel");
+        data 
+            .map( function (o) {
+                let cloneRow = rowTemp.content.cloneNode(true);
 
+                cloneRow.querySelectorAll("tr .tdclass")[0].innerHTML = o.productName;
+                cloneRow.querySelectorAll("tr .tdclass")[1].innerHTML = o.productCode;
+                cloneRow.querySelectorAll("tr .tdclass")[2].innerHTML = o.productAvailable;
+                cloneRow.querySelectorAll("tr .tdclass")[3].innerHTML = o.productPrice;
+                cloneRow.querySelectorAll("tr .tdclass")[4].innerHTML = o.productRating;
+
+                let btnDelete = cloneRow.querySelector("button.delete");
+
+                btnDelete.onclick = function (){
+                    let index = data.indexOf(o);
+                    data.splice(index, 1);
+                    console.log(data);
+                    document.getElementsByTagName("tr")[index+1].remove();
+                }
             return cloneRow;
         })
             .forEach ( function (cloneRow) { return table.appendChild(cloneRow); } );
+            console.log("big");
+    }
+    }
+
+let resizeTimer;
+let oldState;
+let newState;
+
+window.onload = function(){
+    let oldWidth = window.innerWidth;
+    if(oldWidth <= 500)
+    {
+        oldState = "small";
+    }
+    else
+    {
+        oldState = "big";
+    }
+    dataCollect();
+    $(window).one('resize',function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+            let newWidth = window.innerWidth;
+            if(newWidth <= 500)
+                {
+                    newState = "small";
+                }
+            else
+                {
+                    newState = "big";
+                }
+            console.log(`old: ${oldState}`);
+            console.log(`new: ${newState}`);
+            if(oldState !== newState)
+                {
+                    dataCollect();
+                    console.log("resize");
+                }
+            oldState = newState;
+            console.log(`old: ${oldState}`);
+        }, 200);
+      });
 }
--- serverscript.js
+++ serverscript.js
@@ -1,9 +1,7 @@
 // MILESTONE 2
 
 
-window.onload = function() {
-
-      function fetchData() {
+    function fetchData() {
           fetch("http://localhost:3333/products")
           .then( response => {
               if(!response.ok) {
@@ -12,11 +10,53 @@ window.onload = function() {
               return response.json();
           })
           .then( data => {
-              console.log(data);
-              let table = document.querySelector("#tableID tbody");
-              let rowTemp = document.getElementById("rowModel");
-            data 
-                .map( function (o) {
+              
+            if( $( window ).width() <= 500 )
+            {
+                let element = document.createElement('li');
+                element.classList.add("element");
+                let unorderedList = document.getElementById("tableID");
+                unorderedList.appendChild(element);
+                let divTemp = document.getElementById("divModel");
+            data
+            .map( function (o) {
+                let cloneDiv = divTemp.content.cloneNode(true);
+    
+                cloneDiv.querySelectorAll("p")[0].innerHTML = o.productName;
+                cloneDiv.querySelectorAll("p")[1].innerHTML = o.productCode;
+                cloneDiv.querySelectorAll("p")[2].innerHTML = o.releaseDate;
+                cloneDiv.querySelectorAll("p")[3].innerHTML = o.price;
+                cloneDiv.querySelectorAll("p")[4].innerHTML = o.starRating;
+    
+                let btnDelete = cloneDiv.querySelector("button.divDelete");
+    
+                btnDelete.onclick = function (){
+                    let id = o.productId;
+                $.ajax({
+                    url: `http://localhost:3333/product/${id}`,
+                    type: 'DELETE',
+                    success: function(result) {
+                        console.log(`product with Id ${id} deleted`);
+                    }
+                });
+
+                    let index = data.indexOf(o);
+                    data.splice(index, 1);
+                    console.log(data);
+                    document.getElementsByClassName("contentDiv")[index+1].remove();
+                }
+                return cloneDiv;
+            })
+                .forEach ( function (cloneDiv) { return element.appendChild(cloneDiv); } );
+            }
+            
+            else
+            {
+                console.log(data);
+                let table = document.querySelector("#tableID tbody");
+                let rowTemp = document.getElementById("rowModel");
+                data 
+                    .map( function (o) {
             let cloneRow = rowTemp.content.cloneNode(true);
 
             cloneRow.querySelectorAll("tr .tdclass")[0].innerHTML = o.productName;
@@ -45,10 +85,47 @@ window.onload = function() {
 
             return cloneRow;
         })
-            .forEach ( function (cloneRow) { return table.appendChild(cloneRow); } );
+                    .forEach ( function (cloneRow) { return table.appendChild(cloneRow); } );
+            }
+
+
           }).catch( error => {
               console.log(error);
           });
         }
-        fetchData();
+        
+window.onload = function(){
+    let oldWidth = window.innerWidth;
+    if(oldWidth <= 500)
+    {
+        oldState = "small";
+    }
+    else
+    {
+    oldState = "big";
+    }
+    fetchData();
+    $(window).one('resize',function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+        let newWidth = window.innerWidth;
+        if(newWidth <= 500)
+        {
+            newState = "small";
+        }
+        else
+        {
+            newState = "big";
+        }
+        console.log(`old: ${oldState}`);
+        console.log(`new: ${newState}`);
+        if(oldState !== newState)
+        {
+            fetchData();
+            console.log("resize");
+        }
+        oldState = newState;
+        console.log(`old: ${oldState}`);
+        }, 200);
+        });
     }
